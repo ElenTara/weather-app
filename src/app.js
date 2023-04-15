2faccb8228ff3a36d0238914bd5867164a52bf1d
Elen Tara <elentara220@gmail.com>
2023-04-15 17:05:10 +0300

    Showing the current weather for chosen city

--- src/app.js
+++ src/app.js
@@ -3,5 +3,23 @@ let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Odessa&appid=${a
 //console.log(apiUrl);
 function displayTemperature(response) {
   console.log(response.data);
+  let currTemp = document.querySelector("#curr-temp");
+  let temperature = Math.round(response.data.main.temp);
+  if (temperature > 0) {
+    temperature = `+${temperature}`; // можно добавить плюс
+  }
+  currTemp.innerHTML = temperature;
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+
+  let currHum = document.querySelector("#curr-hum");
+  currHum.innerHTML = response.data.main.humidity;
+
+  let currWind = document.querySelector("#curr-wind");
+  currWind.innerHTML = Math.round(response.data.wind.speed);
 }
 axios.get(apiUrl).then(displayTemperature);
